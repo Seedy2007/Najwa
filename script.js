6561e708356cbbee02531db4b8bff64db9a0bb8a
Seedy <saeedzuriki@gmail.com>
2025-09-03 16:07:38 +0300

    3.8

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dividerObserver = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add("visible");
+          entry.target.classList.add("active");
           dividerObserver.unobserve(entry.target);
         }
       });
